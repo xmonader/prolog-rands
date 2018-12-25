324a4fa915eb93e90f2b1bfad89c6540c88663cd
Cyril Margorin <cyril@imperx.com>
2018-12-25 10:09:12 +0300

    fix clues

--- crime.pl
+++ crime.pl
@@ -8,13 +8,8 @@ man(george). man(john). man(robert).
 woman(barbara). woman(christine). woman(yolanda).
 person(X):- man(X).
 person(X):- woman(X).
-location(bathroom). location(dining). location(kitchen). location(livingroom). location(pantry). location(study).
-weapon(bag). weapon(firearm). weapon(gas). weapon(knife). weapon(poison). weapon(rope).
 
 uniq_ppl(A,B,C,D,E,F):- person(A), person(B), person(C), person(D), person(E), person(F),  \+A=B, \+A=C, \+A=D, \+A=E, \+A=F, \+B=C, \+B=D, \+B=E, \+B=F, \+C=D, \+C=E, \+C=F, \+D=E, \+D=F, \+E=F.
-
-space:- write(" ").
-writevars(A,B,C,D,E,F):- write("["), write(A), space, write(B), space, write(C), space, write(D), space, write(E), write(F), space, write("]").
 
 writeanswers(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope):- 
     write("Bathroom: "), write(Bathroom), nl,
@@ -30,46 +25,70 @@ writeanswers(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm,
     write("Bag: "), write(Bag), nl,
     write("Poison: "), write(Poison), nl,
     write("Firearm: "), write(Firearm), nl.
-murderer(X) :-
-   uniq_ppl(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study),
-   uniq_ppl(Bag, Firearm, Gas, Knife, Poison, Rope),
 
 % 2. Clue 1: The man in the kitchen was not found with the rope, knife, or bag.
 % Which weapon, then, which was not the firearm, was found in the kitchen?
 
+clue1(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
    man(Kitchen), 
-   \+Kitchen=Rope, \+Kitchen=Knife, \+Kitchen=Bag, \+Kitchen=Firearm,
+   \+Kitchen=Rope, \+Kitchen=Knife, \+Kitchen=Bag, \+Kitchen=Firearm.
 
 % % 3. Clue 2: Barbara was either in the study or the bathroom; Yolanda was in the other.
 % % Which room was Barbara found in?
-    woman(Bathroom), woman(Study), \+christine=Bathroom, \+christine=Study, 
-    \+barbara=Dining, \+barbara=Kitchen, \+barbara=Livingroom, \+barbara=Pantry,
+
+clue2(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    barbara=Bathroom, yolanda=Study.
+clue2(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    barbara=Study, yolanda=Bathroom.
 
 % % 4. Clue 3: The person with the bag, who was not Barbara nor George, was not in the bathroom nor the dining room.
 % % Who had the bag in the room with them?
 
-    \+barbara=Bag, \+george=Bag, \+barbara=Bathroom, \+barbara=Dining, \+Bag=Bathroom, \+Bag=Dining,
+clue3(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    \+barbara=Bag, \+george=Bag, \+Bag=Bathroom, \+Bag=Dining.
    
 % % 5. Clue 4: The woman with the rope was found in the study.
 % % Who had the rope?
-   
-    woman(Rope), Rope=Study,  
+
+clue4(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    woman(Rope), Rope=Study.
 
 % % 6. Clue 5: The weapon in the living room was found with either John or George.
 % % What weapon was in the living room?
-    man(Livingroom), \+Livingroom=robert,
+clue5(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    Livingroom = john.
+clue5(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    Livingroom = george.
+
 % % 7. Clue 6: The knife was not in the dining room.
 % % So where was the knife?
-    \+Knife=Dining,
+clue6(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    \+Knife=Dining.
 
 % % 8. Clue 7: Yolanda was not with the weapon found in the study nor the pantry.
 % % What weapon was found with Yolanda?
-    \+yolanda=Pantry, \+yolanda=Study,
+clue7(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    \+yolanda=Pantry, \+yolanda=Study.
 
 % % 9. Clue 8: The firearm was in the room with George.
 % % In which room was the firearm found?
-    Firearm=george,
+clue8(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope) :-
+    Firearm=george.
 
 % % 10. It was discovered that Mr. Boddy was gassed in the pantry. The suspect found in that room was the murderer.
 % % Who, then, do you point the finger towards?
-Pantry=Gas, Pantry=X, write("KILLER IS :"), write(X), nl, writeanswers(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope).
+murderer(X) :-
+   uniq_ppl(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study),
+   uniq_ppl(Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue1(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue2(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue3(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue4(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue5(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue6(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue7(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   clue8(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope),
+   X = Gas, X = Pantry,
+   
+   write("KILLER IS :"), write(X), nl, 
+   writeanswers(Bathroom, Dining, Kitchen, Livingroom, Pantry, Study, Bag, Firearm, Gas, Knife, Poison, Rope).
